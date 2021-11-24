5fb5d780d9b4ea89aa42d328ae95230d00a13bcc
vladimirs-sapega <vladimirs_sapega@inbox.lv>
2021-11-24 23:00:38 +0200

    code clean up

--- src/main/java/com/example/demo/ProductsController.java
+++ src/main/java/com/example/demo/ProductsController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProductsController {
     @RequestMapping ("/products")
-
     public String products (){
         return "products";
     }
